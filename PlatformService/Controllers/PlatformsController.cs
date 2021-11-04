a1dc29491fc8c6abff9ec1268a7f829d66720ac1
Muhammed Sayyar <sayyar@cloud.cross.cz>
2021-11-04 15:27:06 +0300

    Add project files.

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+        private readonly ICommandDataClient _commandDataClient;
+        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _commandDataClient = commandDataClient;
+        }
+        [HttpGet("GetPlatforms")]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms....");
+
+            var platformItem = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+        [HttpGet("{id}",Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto  platformCreate)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreate);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            _commandDataClient.SendPlatformToCommand(platformReadDto);
+            return CreatedAtAction(nameof(GetPlatformById), new { id = platformReadDto.Id }, platformReadDto);
+        }
+
+    }
+}
---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using PlatformService.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class AppsDbContext:DbContext
+    {
+      
+        public AppsDbContext(DbContextOptions<AppsDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Platform> Platforms { get; set; }
+
+    }
+}
